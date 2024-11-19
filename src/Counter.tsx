6190ea68ad621efb56660497ed2cd651fa188822
Sean <seanselbyy@gmail.com>
2024-11-19 14:47:54 +1300

    Add styling to counter Add reset button

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,16 +1,21 @@
 import {useState} from 'react'
+import './App.css'
 
 function Counter() {
     const [count, setCount] = useState(0);
 
     const increment = () => setCount(count +1);
     const decrement = () => setCount(Math.max(0, count - 1));
+    const reset = () => setCount(0);
 
     return (
-        <div>
-            <p>Count: {count}</p>
-            <button onClick={increment}>Increment</button> 
-            <button onClick={decrement}>Decrement</button> 
+        <div className='counter-container'>
+            <div className='button-container'>
+            <button className='button' onClick={increment}>Increment</button> 
+            <button className='button' onClick={decrement}>Decrement</button>
+            <button className='button' onClick={reset}>Reset</button>
+            </div>
+            <p className='counter'>Count: {count}</p>
         </div>
     );
 }
